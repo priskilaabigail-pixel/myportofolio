ce4bee085d33e5edef222cb795b5b441d2c21c82
Priskila Abigail Magaini <priskila.abigail@ui.ac.id>
2026-09-06 20:05:22 +0700

    centering education section

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from dotenv import load_dotenv
 # Load environment variables from .env file
+
 load_dotenv()
 
 # konfigurasi ALLOWED_HOSTS and PRODUCTION
