7c223d8a96ad1e7b7936ac2a1e67571d30cf2d6f
Priskila Abigail Magaini <priskila.abigail@ui.ac.id>
2026-09-14 16:01:04 +0700

    menambahkan trusted csrf di settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -150,3 +150,5 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ['https://priskila-abigail-myportofolio.pws.cs.ui.ac.id/']
