17030740ed80c5376f87174f36cc43d2bf019cc2
Priskila Abigail Magaini <priskila.abigail@ui.ac.id>
2026-09-16 21:11:51 +0700

    Tutorial 03

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_certification
+from main.views import show_main, show_experience, show_certification, create_project, show_projects, get_projects_json, delete_project
 
 app_name = "main"
 
@@ -8,4 +8,8 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("certification/", show_certification, name="show_certification"),
+    path("projects/add/", create_project, name="create_project"),
+    path("projects/", show_projects, name="show_projects"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project"),
 ]
